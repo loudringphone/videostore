ecab6e07c0461f5185bafd8164f39a0bcde5cb5b
loudringphone <loudringphone@gmail.com>
2023-04-24 10:34:07 +1000

    scroll to top after redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import './App.css';
 
 import { Layout } from './components/layout/Layout';
 
 function App() {
- 
-  return (
-    <Layout />
-  );
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+
+      return (
+        <Layout />
+      );
 }
  
 export default App;
