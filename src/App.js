d33435a82c1dea65998f7558111aa0603cdf9c0b
loudringphone <loudringphone@gmail.com>
2023-04-24 10:52:21 +1000

    scroll to top after redirect isntantly

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ import { Layout } from './components/layout/Layout';
 
 function App() {
 
-    const { pathname } = useLocation();
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
-
       return (
-        <Layout />
+        <div className="App">
+            <ScrollToTop />
+            <Layout />
+        </div>
       );
 }
  
 export default App;
+
+
+
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        document.documentElement.scrollTo({
+        // In general, if you want to scroll to the top of the page, using document.documentElement.scrollTop is the more reliable and widely used approach. However, if you need to scroll to a specific position on the page or use more advanced scrolling behaviors, window.scrollTo or window.scrollBy may be more appropriate.
+        top: 0,
+        left: 0,
+        behavior: "instant",
+        });
+    }, [pathname]);
+
+    return null;
+}
