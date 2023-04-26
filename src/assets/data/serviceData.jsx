6479b0a91170aa08200f45be0b929db8045913b7
loudringphone <loudringphone@gmail.com>
2023-04-26 20:57:46 +1000

    over 10 products to choose from

--- src/assets/data/serviceData.jsx
+++ src/assets/data/serviceData.jsx
@@ -3,25 +3,21 @@ const serviceData = [
         icon: "ri-truck-line",
         title: "Free AU Shipping",
         subtitle: "on all products",
-        bg: "antiquewhite"
     },
     {
         icon: "ri-box-3-line",
-        title: "Over 100 Products",
+        title: "Over 10 Products",
         subtitle: "to choose from",
-        bg: "paleturquoise"
     },
     {
         icon: "ri-secure-payment-line",
         title: "Secure Payment",
         subtitle: "using PayPal",
-        bg: "pink"
     },
     {
         icon: "ri-exchange-dollar-line",
         title: "Money Back Guarantee",
         subtitle: "if our supplier accepts the return",
-        bg: "lightgoldenrodyellow"
     },
 ]
 
